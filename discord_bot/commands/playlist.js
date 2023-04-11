0dfc3dc236b9f4b99fb5ba71425a9f7126255c5f
marcelo de souza rodrigues <marcelo@pop-os.localdomain>
2023-04-11 16:58:56 -0300

    update

--- discord_bot/commands/playlist.js
+++ discord_bot/commands/playlist.js
@@ -7,6 +7,6 @@ module.exports = {
         .setDescription("Minha playlist do spotify"),
 
     async execute(interaction) {
-        await interaction.reply("https://open.spotify.com/playlist/222R5ewKDe6P2teS7yL12C?si=c44f2f488938408a")
+        await interaction.reply("playlistlink")
     }
 }
